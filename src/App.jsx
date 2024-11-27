7c150256c259c267368ad650134e89dcaacfaad7
Dip <65559847+Dip-Barua@users.noreply.github.com>
2024-11-27 16:33:23 +0600

    Added : Ui Input functionalities with post data to server side

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+
+import './App.css'
+
+function App() {
+
+const handleAddUser = event => {
+  event.preventDefault();
+
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const user = {name , email};
+  console.log(user);
+
+
+  fetch('http://localhost:5000/users',
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user),  // Convert the object to JSON string before sending it.
+    }
+   )
+   .then(response => response.json())
+   .then(data => {
+    console.log(data);
+   })
+
+};
+
+
+
+  return (
+    <>
+      
+      <h1>Simple Crud</h1>
+      
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Enter Name" />
+        <br />
+        <input type="email" name="email" placeholder="Enter Email" />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+
+    </>
+  )
+}
+
+export default App
